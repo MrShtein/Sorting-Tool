50321ca8fc871272f5de55df1addfc4461a22fd3
Mr.Shtein <mr.shtein61@yandex.ru>
2021-03-10 17:17:08 +0300

    Completed stage 1

--- sorting/Main.java
+++ sorting/Main.java
@@ -5,10 +5,24 @@ import java.util.*;
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Long> numberList = new ArrayList<>();
+        long maxValue = 0L;
+        long times = 0L;
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
-            // write your code here
+            if (number > maxValue) {
+                maxValue = number;
+            }
+            numberList.add(number);
         }
+
+        for (Long num : numberList) {
+            if (num == maxValue) {
+                times++;
+            }
+        }
+
+        System.out.printf("Total numbers: %d\n", numberList.size());
+        System.out.printf("The greatest number: %d (%d time(s)).", maxValue, times);
     }
 }
